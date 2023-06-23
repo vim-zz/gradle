030ce0ce8f1c7f9d24fe7bf580ad189e72d21dcf
Reinhold Degenfellner <rdegenfellner@gradle.com>
2023-06-23 13:56:12 +0200

    Use new Problems API for property

--- subprojects/core/src/main/java/org/gradle/api/internal/tasks/properties/DefaultPropertyValidationContext.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/tasks/properties/DefaultPropertyValidationContext.java
@@ -46,6 +46,11 @@ public class DefaultPropertyValidationContext implements PropertyValidationConte
     }
 
     @Override
+    public void visitPropertyNewProblem(Action<? super TypeAwareProblemBuilder> problemSpec) {
+        delegate.visitPropertyNewProblem(problemSpec);
+    }
+
+    @Override
     public PathToFileResolver getFileResolver() {
         return fileResolver;
     }
